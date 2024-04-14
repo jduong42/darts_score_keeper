edee237ed7b1dd4ddf2ad3d7fc82cedb640b75f6
Jiaoaqua <157594316+Jiaoaqua@users.noreply.github.com>
2024-04-14 17:20:31 +0300

    Update App.js with game settings component

--- dart_score_keeper/src/App.js
+++ dart_score_keeper/src/App.js
@@ -1,21 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+
+// Importing GameSettingsComponent - Jiao's component for game settings
 import GameSettingsComponent from './components/GameSettingsComponent';
 
 function App() {
 
+  // Handle settings submission - Added by Jiao to process game settings data
   const handleSettingsSubmit = (settings) => {
     console.log('Settings submitted:', settings);
+
+    // Additional handling can be implemented here
   };
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Edit <code>src/App.js</code> and save to reload.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -25,10 +28,13 @@ function App() {
           Learn React
         </a>
       </header>
+
+      {/* Game settings form component - Jiao */}
       <GameSettingsComponent onSubmit={handleSettingsSubmit} />
     </div>
   );
 }
 
 export default App;
+
 
