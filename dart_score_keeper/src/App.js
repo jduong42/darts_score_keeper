08bfa51ca438af2fd0f66f3c73c18f3f5ee84639
Eerik <ekvainio@hotmail.com>
2024-04-24 12:42:10 +0300

    Revert "Merge branch 'game-settings-feature'"
    This reverts commit a7dae71e62193b10faca95ccc7f67606be743b43, reversing
    changes made to 2f185bf95b3d032e6d7a672a7aff6044fb473b4b.

--- dart_score_keeper/src/App.js
+++ dart_score_keeper/src/App.js
@@ -10,23 +10,10 @@ import { AddScore } from './components/AddScore.js';
 import { GlobalProvider } from './context/GlobalState';
 
 import './App.css';
-import React from 'react';
-
-// Importing GameSettingsComponent - Jiao's component for game settings
-import GameSettingsComponent from './components/GameSettingsComponent';
 
 
 function App() {
-
-  // Handle settings submission - Added by Jiao to process game settings data
-  const handleSettingsSubmit = (settings) => {
-    console.log('Settings submitted:', settings);
-
-    // Additional handling can be implemented here
-  };
-
   return (
-<<<<<<< HEAD
     <GlobalProvider>
       <Header />
       <div className="container">
@@ -38,28 +25,6 @@ function App() {
       </div>
     </GlobalProvider>
     );
-=======
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Edit <code>src/App.js</code> and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-
-      {/* Game settings form component - Jiao */}
-      <GameSettingsComponent onSubmit={handleSettingsSubmit} />
-    </div>
-  );
->>>>>>> game-settings-feature
 }
 
 export default App;
-
-
